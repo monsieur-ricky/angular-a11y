88ca0e987a875a99581e5eb062dfc3cefdc2e7dd
Ricky <ricky2k7@gmail.com>
2025-04-01 18:33:27 +0200

    feat: Show dialog when adding item to cart

--- src/app/posters/posters.component.ts
+++ src/app/posters/posters.component.ts
@@ -1,17 +1,21 @@
-import { Component, inject, resource } from '@angular/core';
+import { Component, inject, resource, signal } from '@angular/core';
 import { Poster } from '../shared/models/product.interface';
 import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../shared/services/cart.service';
+import { DialogComponent } from '../shared/ui/dialog/dialog.component';
 
 @Component({
   selector: 'app-posters',
-  imports: [RouterModule, CurrencyPipe, NgOptimizedImage],
+  imports: [RouterModule, CurrencyPipe, NgOptimizedImage, DialogComponent],
   templateUrl: './posters.component.html',
   styleUrl: './posters.component.scss'
 })
 export class PostersComponent {
   private readonly cartService = inject(CartService);
+
+  showDialog = signal(false);
+  dialogMessage = signal('');
 
   productsResource = resource({
     loader: () =>
@@ -20,5 +24,7 @@ export class PostersComponent {
 
   addToCart(poster: Poster) {
     this.cartService.addItem(poster);
+    this.dialogMessage.set(`Added the ${poster.name} poster to the cart.`);
+    this.showDialog.set(true);
   }
 }
